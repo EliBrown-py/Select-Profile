7dbfbe2a1e2c9773749ed1363f2bf8ce89e42000
Elijah Ibidayo <83242370+EliBrown-py@users.noreply.github.com>
2023-10-19 12:28:19 -0400

    Add files via upload

---
+++ selectprofile.js
@@ -0,0 +1,8 @@
+
+const esterblishProfile =() => {
+    $("#name").text("Eliah Ibidayo")
+    $("#major").text("CIT Web-development");
+    $("#year").text("2023");
+    $("h4").html("<em>Nigeria</em>");
+};
+$(document).ready(esterblishProfile);
